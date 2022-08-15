c50bf97b7ef4bb25f10e1d369cdf58f0a5678a27
Mali3454 <wojtek.buchwal@gmail.com>
2022-08-15 13:59:09 +0200

    feat: Implement of app store and request helper

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './App.scss'
-
-const App = () => <header>Hello </header>
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import StoreProvide from './store/StroreProvider'
+
+import './App.scss'
+
+const App = () => (
+	<StoreProvider>
+		<header>Hello </header>
+	</StoreProvider>
+)
+export default App
+
+
